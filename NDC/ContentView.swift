6c141244739c848a711ad81fe2860ed153e393d7
Matthew Goli <admin@newdigitalcommune.com>
2023-12-13 13:43:20 -0800

    Initial Commit

---
+++ NDC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NDC
+//
+//  Created by Matthew J Goli on 12/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NDC/NDCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDCApp.swift
+//  NDC
+//
+//  Created by Matthew J Goli on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct NDCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
